08ae1dcb0a5fb9555a0fdb2ff877d647a192bf8f
Tejas Arlimatti <tejasarlimatti@gmail.com>
2019-11-19 02:09:32 +0530

    Added product image and product tag model

--- main/models.py
+++ main/models.py
@@ -10,3 +10,16 @@ class Product(models.Model):
     active       = models.BooleanField(default=True)
     in_stock     = models.BooleanField(default=True)
     date_updated = models.DateTimeField(auto_now=True)
+
+class ProductImage(models.Model):
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image   = models.ImageField(upload_to='product-images')
+
+class ProductTags(models.Model):
+
+    products    = models.ManyToManyField(Product, blank=True)
+    name        = models.CharField(max_length=32)
+    slug        = models.SlugField(max_length=48)
+    description = models.TextField(blank=True)
+    active      = models.BooleanField(default=True)
