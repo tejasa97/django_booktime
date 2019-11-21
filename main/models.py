a38ea4e5dcb3ecd78058e6e1b14d721b44c5aa3c
Tejas Arlimatti <tejas.arlimatti@saankhyalabs.com>
2019-11-21 16:40:12 +0530

    Added signal for thumnail generation

--- main/models.py
+++ main/models.py
@@ -13,8 +13,9 @@ class Product(models.Model):
 
 class ProductImage(models.Model):
 
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    image   = models.ImageField(upload_to='product-images')
+    product   = models.ForeignKey(Product , on_delete=models.CASCADE)
+    image     = models.ImageField(upload_to ='product-images')
+    thumbnail = models.ImageField(upload_to ='product-thumbnails', null=True)
 
 class ProductTags(models.Model):
 
---
+++ main/tests/test_signals.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from main import models
+from django.core.files.images import ImageFile
+from decimal import Decimal
+
+# class TestSignal(TestCase):
+
+#     def test_thumbnails_are_generated_on_save(self):
+
+#         product = models.Product(
+#             name  ="The cathedral and the bazaar",
+#             price = Decimal("10.00")
+#         )
+#         product.save()
+
+
+
